ba3dd466ad75393bbb5e6fa8914ff67174c17ad1
estidlore <estidlore@outlook.com>
2022-10-08 15:24:49 -0500

    feat(front): #1 text styles
    - Style for paragraphs and headers
    - Typing for colors' keys

--- src/styles/index.tsx
+++ src/styles/index.tsx
@@ -1,3 +1,17 @@
+import { StyleSheet } from "react-native";
+
 import { colors } from "./colors";
 
-export { colors };
+const styles =  StyleSheet.create({
+  p: {
+    color: colors.BLACK,
+    fontSize: 18,
+  },
+  h: {
+    color: colors.BLACK,
+    fontSize: 24,
+    fontWeight: "700",
+  },
+});
+
+export { colors, styles };
