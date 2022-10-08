4d98bb6ade39b3bc851ac7da150e7af502cced2b
estidlore <estidlore@outlook.com>
2022-10-07 22:49:42 -0500

    absolute paths

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        alias: {
+          "assets/*": ["assets/*"],
+          "components/*": ["components/*"],
+          "navigation/*": ["navigation/*"],
+          "styles/*": ["styles/*"],
+          "utils/*": ["utils/*"],
+          "views/*": ["views/*"],
+        },
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+        root: ["src"],
+      },
+    ],
+  ],
 };
