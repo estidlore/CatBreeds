f758ef6fe28cb017b00039fb3d78014254918d88
estidlore <estidlore@outlook.com>
2022-10-09 14:10:57 -0500

    feat(front): #2 add colors and center header
    - Add some gray-scale colors
    - Center text of header style

--- src/styles/colors.tsx
+++ src/styles/colors.tsx
@@ -1,7 +1,18 @@
-type TColor = "BLACK" | "WHITE";
+type TColor = "BLACK" |
+  "DARK" |
+  "GRAY" |
+  "GRAY_DARK" |
+  "GRAY_LIGHT" |
+  "LIGHT" |
+  "WHITE";
 
 const colors: Record<TColor, string> = {
   BLACK: "#040404",
+  DARK: "#202020",
+  GRAY: "#808080",
+  GRAY_DARK: "#404040",
+  GRAY_LIGHT: "#C0C0C0",
+  LIGHT: "#E0E0E0",
   WHITE: "#FBFBFB",
 };
 
