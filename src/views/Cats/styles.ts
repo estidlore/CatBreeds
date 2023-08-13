d2796d41b49fb273ebfce1dfbca7d28c62373046
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-08-13 16:16:14 -0500

    feat: #4 add view cats
    - Add view Cats to list cats information

---
+++ src/views/Cats/styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  card: {
+    marginVertical: 8,
+  },
+  container: {
+    padding: 8,
+  },
+  loading: {
+    marginTop: 8,
+    fontSize: 16,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingVertical: 8,
+  },
+});
+
+export { styles };
